f92cab14e16ac801be75042cee7bc65867940c27
Saakshi Joshi <89957213+cinnamonhater@users.noreply.github.com>
2023-01-18 18:26:00 -0700

    Update echo_server.py

--- echo_server.py
+++ echo_server.py
@@ -1,4 +1,5 @@
 import socket
+from threading import Thread
 
 BYTES_TO_READ = 4096
 HOST = "127.0.0.1" #or 0.0.0.0 which binds the socket to every single address on my computer
@@ -7,7 +8,7 @@ PORT = 8080
 #call when we receive a connection
 def handle_connection(conn, addr):
     with conn:
-        print("Connected by: {addr}")
+        print(f"Connected by: {addr}")
         while True:
 
             data = conn.recv(BYTES_TO_READ)
@@ -15,7 +16,6 @@ def handle_connection(conn, addr):
                 break
             print(data)
             conn.send(data) #send returns no. of bytes it sent. send_all returns an error if it doesnt send everything
-    return
 
 def start_server():
     #with blocks help to autoclean code by closing stuff we might have overlooked. Don't close here cuz with will do that for you
@@ -28,6 +28,22 @@ def start_server():
 
         conn, addr = s.accept() #returns clients IP address and port as a tuple
         handle_connection(conn, addr)
-    return
+        
+# Start multithreaded echo server
+def start_threaded_server():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST,PORT))
+
+        '''
+        setsockopt(level, option, value)
+        https://manpages.debian.org/bullseye/manpages-dev/setsockopt.2.en.html
+        '''
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.listen(2) # Allow backlog of up to 2 connections
+        while True:
+            conn, addr = s.accept()
+            thread = Thread(target=handle_connection, args=(conn, addr))
+            thread.run()
 
 start_server()
+start_threaded_server()
