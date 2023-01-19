467d2f6cd0c069b03036ca14effcf3d3ae701ab9
Saakshi Joshi <89957213+cinnamonhater@users.noreply.github.com>
2023-01-18 18:26:47 -0700

    Update proxy_client.py

--- proxy_client.py
+++ proxy_client.py
@@ -1,11 +1,23 @@
 import socket
 
-BYTES_TO_READ
+BYTES_TO_READ = 4096
 
 def get(host, port):
-    request_data = b"GET / HTTP1.1\nHost: www.google.com\n\n"
-
+    request = b"GET / HTTP/1.1\nHost: www.google.com\n\n"
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        pass
-    #COMPLETE THIS PAGE. CODE IN PHONE GALLERY
+        s.connect((host,port))
+        s.send(request)
+        # Shut the socket to further writes
+        s.shutdown(socket.SHUT_WR)
+        
+        print("Waiting for response!")
+        chunk = s.recv(BYTES_TO_READ)
+        result = b'' + chunk
+        
+        while(len(chunk)>0):
+            chunk = s.recv(BYTES_TO_READ)
+            result += chunk
+        s.close()
+        return result
 
+print(get("127.0.0.1", 8080))
